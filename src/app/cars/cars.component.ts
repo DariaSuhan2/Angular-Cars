5fcfc5cb7239fdb7ad8b6e7b306f6d18490e3c35
DariaSuhan <daria.suhan@tooktech.com>
2023-12-20 15:00:01 +0200

    step 4 - added services (tbd observable data)

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {CARS, ICar} from "../models/car";
+import { CarService } from '../service/car.service';
 
 @Component({
   selector: 'app-cars',
@@ -7,11 +8,21 @@ import {CARS, ICar} from "../models/car";
   styleUrl: './cars.component.css'
 })
 export class CarsComponent {
-  cars = CARS;
+  //cars = CARS;
   selectedCar?: ICar;
+  cars: ICar[] = [];
+  constructor (private carService: CarService) {}
   
 
   onSelect(car: ICar): void {
     this.selectedCar = car;
+  }
+
+  getCars(): void {
+    this.cars = this.carService.getCars();
+  }
+
+  ngOnInit(): void {
+    this.getCars();
   }
 }
